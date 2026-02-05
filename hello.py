75099db9d00a13aa117ae9765adee0ef702b7735
Noel Ninan Sheri <noelninansheri@gmail.com>
2026-02-05 23:45:29 +0530

    Source code change for checking jenkins auto build on github code change

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
 print("This is the python source file hello.py within the repo: NoelNinanSheri-JenkinsLab being run via Jenkins Automation\nNoel Ninan Sheri")
+print("For checking Jenkins Integration with GitHub Repo and Auto Builds")
+print("Noel Ninan Sheri")
+print("23MIS0026")
